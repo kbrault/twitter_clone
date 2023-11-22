f77d21f787a6831f989052bb98fe722078745d19
Kevin <kbrault@outlook.fr>
2023-11-22 15:27:39 +0100

    add database feature

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use actix_web::{get, web, App, HttpServer, middleware::Logger, Responder, Result};
-use chrono::{DateTime, Utc};
-use serde::{Serialize, Deserialize};
+use actix_web::{get, middleware::Logger, web, App, HttpServer, Responder, Result};
+use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
+use dotenvy;
+use serde::{Deserialize, Serialize};
+use sqlx::{Row, SqlitePool};
 use std::{sync::Mutex, vec};
 use uuid::Uuid;
 
 type Tweets = Response<Tweet>;
 
 struct AppState {
-    app_name : String,
-    counter: Mutex<i32>
+    app_name: String,
+    counter: Mutex<i32>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -19,20 +21,21 @@ struct Tweet {
     likes: Vec<Like>,
 }
 
-impl Tweet{
-    fn new(message: String) -> Self{
-        Self { 
-            id: Uuid::new_v4().to_string(), 
-            date: Utc::now(), 
-            message, 
-            likes: vec![] }
+impl Tweet {
+    fn new(message: String) -> Self {
+        Self {
+            id: Uuid::new_v4().to_string(),
+            date: Utc::now(),
+            message,
+            likes: vec![],
+        }
     }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct Like{
+struct Like {
     id: String,
-    date: DateTime<Utc>
+    date: DateTime<Utc>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -40,42 +43,64 @@ struct Response<T> {
     results: Vec<T>,
 }
 
-
-
 #[get("/")]
 async fn index(data: web::Data<AppState>) -> String {
     let app_name = &data.app_name;
-    let mut counter = data.counter.lock().unwrap(); 
+    let mut counter = data.counter.lock().unwrap();
     *counter += 1;
     format!("{app_name} was visited {counter} times this run.")
 }
 
 #[get("/tweets")]
-pub async fn list() -> Result<impl Responder> {
-    let mut tweets: Response<Tweet> = Tweets {results: vec![]};
-    
-    let tweet1 = Tweet::new("tweet_1".to_string());
-    let tweet2 = Tweet::new("tweet_2".to_string());
-    let tweet3 = Tweet::new("tweet_3".to_string());
+pub async fn list(db: web::Data<SqlitePool>) -> Result<impl Responder> {
+    let mut tweets: Response<Tweet> = Tweets { results: vec![] };
 
-    tweets.results.push(tweet1);
-    tweets.results.push(tweet2);
-    tweets.results.push(tweet3);
+    let result = sqlx::query("SELECT * FROM tweets")
+        .fetch_all(db.as_ref())
+        .await;
 
-    Ok(web::Json(tweets))
+    match result {
+        Ok(rows) => {
+            for row in &rows {
+                let date_db = row.get("date");
+                let parsed_naive = NaiveDateTime::parse_from_str(date_db, "%Y-%m-%dT%H:%M:%S%.fZ")
+                    .expect("Failed to parse date/time string");
+                let datetime_utc = Utc.from_utc_datetime(&parsed_naive);
+                let to_add = Tweet {
+                    id: row.get("id"),
+                    date: datetime_utc,
+                    message: row.get("message"),
+                    likes: vec![],
+                };
+                tweets.results.push(to_add)
+            }
+            Ok(web::Json(tweets))
+        }
+        Err(e) => {
+            panic!("Error : {}", e);
+        }
+    }
 }
 
+// TODO : #[post("/tweets")]
+// API POST/tweets --> add a tweet to database
+
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {  
+async fn main() -> std::io::Result<()> {
+    let database_url = dotenvy::var("DATABASE_URL").unwrap();
+
     let app = web::Data::new(AppState {
         app_name: String::from("Twitter Clone"),
-        counter: Mutex::new(0)
+        counter: Mutex::new(0),
     });
+
+    let pool = SqlitePool::connect(&database_url).await.unwrap();
 
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
     HttpServer::new(move || {
         App::new()
             .app_data(app.clone())
+            .app_data(web::Data::new(pool.clone()))
             .service(index)
             .service(list)
             .wrap(Logger::new("%a %r %s %b %{Referer}i %{User-Agent}i %Ts"))
